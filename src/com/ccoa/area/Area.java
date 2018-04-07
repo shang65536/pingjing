c0732c4e6ba8a2da67dfd91bdcd51f7d1c28191c
xukai <13811960623@139.com>
2018-04-07 14:15:54 +0800

    增加获取地区列表方法

---
+++ src/com/ccoa/area/Area.java
@@ -0,0 +1,20 @@
+package com.ccoa.area;
+
+import com.jfinal.plugin.activerecord.Model;
+
+import java.util.List;
+
+public class Area extends Model<Area> {
+
+    public static final Area me = new Area();
+
+    /**
+     * 获取地区列表
+     * @return
+     */
+    public List<Area>  getArea()
+    {
+        String sql="select id, AeeaName from set_area order by OrderIndex";
+        return this.find(sql);
+    }
+}
---
+++ src/com/ccoa/area/AreaController.java
@@ -0,0 +1,15 @@
+package com.ccoa.area;
+
+import com.jfinal.core.Controller;
+
+import java.util.List;
+
+public class AreaController extends Controller {
+    /**
+     * 获取地区列表
+     */
+    public void getArea() {
+        List<Area> areas = Area.me.getArea();
+        setAttr("areas", areas);
+    }
+}
