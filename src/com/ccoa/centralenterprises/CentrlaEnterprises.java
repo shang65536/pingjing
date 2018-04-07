082e9850fbab7721292b0d9f27039c11924aff2b
xukai <13811960623@139.com>
2018-04-07 14:51:17 +0800

    增加央企数据列表获取方法

---
+++ src/com/ccoa/centralenterprises/CentrlaEnterprises.java
@@ -0,0 +1,25 @@
+package com.ccoa.centralenterprises;
+
+import com.jfinal.plugin.activerecord.Model;
+
+import java.util.List;
+
+public class CentrlaEnterprises extends Model<CentrlaEnterprises> {
+    public static final CentrlaEnterprises me = new CentrlaEnterprises();
+
+    /**
+     * 检索央企数据
+     *
+     * @param param
+     * @return
+     */
+    public List<CentrlaEnterprises> search(String param) {
+        StringBuilder sql = new StringBuilder();
+        sql.append(" select id ,CompanyName from set_centrlaenterprises where 1=1 ");
+        if (param != null) {
+            sql.append("    sql.append(CompanyName like '%" + param + "%'");
+        }
+        sql.append(" order by OrderIndex");
+        return this.find(sql.toString());
+    }
+}
---
+++ src/com/ccoa/centralenterprises/CentrlaEnterprisesController.java
@@ -0,0 +1,18 @@
+package com.ccoa.centralenterprises;
+
+import com.jfinal.core.Controller;
+
+import java.util.List;
+
+public class CentrlaEnterprisesController extends Controller {
+
+    /**
+     * 获取央企列表
+     */
+   public  void getCentralEnerprises()
+   {
+       String param = getPara("param");
+      List<CentrlaEnterprises> centrlaEnterprises=CentrlaEnterprises.me.search(param);
+       setAttr("CentrlaEnterprises",centrlaEnterprises);
+   }
+}
