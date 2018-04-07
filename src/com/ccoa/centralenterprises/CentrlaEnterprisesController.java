5c63e55e5e088c69d80ecd185c551734a6b1265a
xukai <13811960623@139.com>
2018-04-07 14:55:15 +0800

    格式化代码

--- src/com/ccoa/centralenterprises/CentrlaEnterprisesController.java
+++ src/com/ccoa/centralenterprises/CentrlaEnterprisesController.java
@@ -9,10 +9,9 @@ public class CentrlaEnterprisesController extends Controller {
     /**
      * 获取央企列表
      */
-   public  void getCentralEnerprises()
-   {
-       String param = getPara("param");
-      List<CentrlaEnterprises> centrlaEnterprises=CentrlaEnterprises.me.search(param);
-       setAttr("CentrlaEnterprises",centrlaEnterprises);
-   }
+    public void getCentralEnerprises() {
+        String param = getPara("param");
+        List<CentrlaEnterprises> centrlaEnterprises = CentrlaEnterprises.me.search(param);
+        setAttr("CentrlaEnterprises", centrlaEnterprises);
+    }
 }
